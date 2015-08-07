addf22b94626345a0bff8e44848b1c58ea039ea5
Vladimir Yunev <vyunev@live.ru>
2015-08-07 10:51:12 +0300

    First Commit

---
+++ ViewDemo/src/ViewDemo/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace ViewDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
